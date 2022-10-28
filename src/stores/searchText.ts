199af6b131251a1fa9c4556a6f93a7b7ff9efc17
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-10-28 12:53:24 +0300

    delte usless comments

--- src/stores/searchText.ts
+++ src/stores/searchText.ts
@@ -2,16 +2,6 @@ import {defineStore} from 'pinia'
 import {Ref, ref} from "vue";
 
 
-
-
-// export const useSearchStore = defineStore("search1", {
-//     state: () => ({search: ref("")}),
-//     actions: {
-//         changeText(searchedText: string) {
-//             this.search.value = searchedText;
-//         }
-//     }
-// })
 export const useSearchStore = defineStore("search", ()=>{
     const search = ref("");
     return{search}
