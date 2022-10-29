58c23572329032de5599e5474e1798dcb6edd853
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-10-29 15:19:46 +0300

    add obj of chosen currency and method that change obj

--- src/stores/chosenCurrency.ts
+++ src/stores/chosenCurrency.ts
@@ -1,8 +1,29 @@
 import {defineStore} from 'pinia'
-import {Ref, ref} from "vue";
+import {ref} from "vue";
+import {useValutesStore} from "@/stores/valutes";
 
 
-export const useChosenCurrencyStore = defineStore("chosenCurrency", ()=>{
+export const useChosenCurrencyStore = defineStore("chosenCurrency", () => {
     const chosenCurrency = ref("RUB");
-    return{chosenCurrency}
+    let chosenCurrencyObj = ref({
+        ID:"R01090R",
+        NumCode:"810",
+        CharCode:"RUB",
+        Nominal:1,
+        Name:"Российский рубль",
+        Value:1,
+        Previous:1
+    });
+
+    const valutesStore = useValutesStore();
+
+    function getChosenCurrencyObj(): void {
+        for (let i of valutesStore.valutes) {
+            if (i.CharCode === chosenCurrency.value) {
+                chosenCurrencyObj.value = i;
+            }
+        }
+    }
+
+    return {chosenCurrency, getChosenCurrencyObj, chosenCurrencyObj}
 })
