913d4d76e14f0b34e99f504f30a44c5f3465e484
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-10-30 22:24:26 +0300

    change generation of valutes

--- src/components/converter/main/TheMainHave.vue
+++ src/components/converter/main/TheMainHave.vue
@@ -2,11 +2,10 @@
   <div class="item">
     <h2 class="have">У меня есть</h2>
     <div class="valutes-buttons">
-      <the-main-have-valute standard-valute="RUB" leftborder="true"></the-main-have-valute>
-      <the-main-have-valute standard-valute="USD"></the-main-have-valute>
-      <the-main-have-valute standard-valute="EUR"></the-main-have-valute>
-      <the-main-have-valute standard-valute="JPY"></the-main-have-valute>
-      <the-main-have-choose rightborder="true"></the-main-have-choose>
+      <template v-for="currency in currencysStore.currencysHave" :key="currency">
+        <the-main-have-valute :valute="currency"></the-main-have-valute>
+      </template>
+      <the-main-have-choose></the-main-have-choose>
     </div>
     <the-main-have-enter></the-main-have-enter>
   </div>
@@ -16,6 +15,9 @@
 import TheMainHaveValute from "@/components/converter/main/TheMainHaveValute.vue"
 import TheMainHaveEnter from "@/components/converter/main/TheMainHaveEnter.vue"
 import TheMainHaveChoose from "@/components/converter/main/TheMainHaveChoose.vue"
+import {useCurrencysStore} from "@/stores/currencys";
+
+let currencysStore = useCurrencysStore();
 </script>
 
 <style scoped lang="scss">
@@ -24,7 +26,8 @@ import TheMainHaveChoose from "@/components/converter/main/TheMainHaveChoose.vue
   font-size: 20px;
 
 }
-.have{
+
+.have {
   font-size: 25px;
 }
 </style>
--- src/components/converter/main/TheMainWant.vue
+++ src/components/converter/main/TheMainWant.vue
@@ -2,11 +2,10 @@
   <div class="item">
     <h2 class="want">Хочу получить</h2>
     <div class="valutes-buttons">
-      <the-main-want-valute standard-valute="RUB" leftborder="true"></the-main-want-valute>
-      <the-main-want-valute standard-valute="USD"></the-main-want-valute>
-      <the-main-want-valute standard-valute="EUR"></the-main-want-valute>
-      <the-main-want-valute standard-valute="JPY"></the-main-want-valute>
-      <the-main-want-choose rightborder="true"></the-main-want-choose>
+      <template v-for="currency in currencysStore.currencysWant" :key="currency">
+        <the-main-want-valute :valute="currency"></the-main-want-valute>
+      </template>
+      <the-main-want-choose></the-main-want-choose>
     </div>
     <the-main-want-result></the-main-want-result>
   </div>
@@ -16,15 +15,18 @@
 import TheMainWantValute from "@/components/converter/main/TheMainWantValute.vue"
 import TheMainWantResult from "@/components/converter/main/TheMainWantResult.vue"
 import TheMainWantChoose from "@/components/converter/main/TheMainWantChoose.vue"
+import {useCurrencysStore} from "@/stores/currencys";
+
+let currencysStore = useCurrencysStore();
 </script>
 
 <style scoped>
-.want{
+.want {
   font-size: 25px;
 }
+
 .valutes-buttons {
   display: flex;
   font-size: 20px;
-
 }
 </style>
